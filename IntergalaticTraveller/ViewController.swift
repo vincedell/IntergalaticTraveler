6e00ee929a9578bd7e000ee36f00d79a6e408f4a
vincvincedell <vince.dell3@gmail.com>
2020-10-21 09:12:32 -0400

    Initial Commit

---
+++ IntergalaticTraveller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntergalaticTraveller
+//
+//  Created by Vincent Dell'Arciprete on 10/21/20.
+//  Copyright © 2020 Vincent Dell'Arciprete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
